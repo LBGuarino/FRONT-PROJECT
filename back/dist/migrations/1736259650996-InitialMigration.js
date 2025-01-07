834e59722254ec0ce31ae1f7eb64aa2a98c81397
lbguarino <lbelen2014@gmail.com>
2025-01-07 15:23:45 +0100

    FINAL FINAL COMMIT

--- back/dist/migrations/1736259650996-InitialMigration.js
+++ back/dist/migrations/1736259650996-InitialMigration.js
@@ -1,8 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class InitialMigration implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.InitialMigration1736259650996 = void 0;
+class InitialMigration1736259650996 {
+    up(queryRunner) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield queryRunner.query(`
       CREATE TABLE categories (
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL
@@ -49,10 +60,11 @@ export class InitialMigration implements MigrationInterface {
         quantity INTEGER NOT NULL
       );
     `);
-  }
-
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
+        });
+    }
+    down(queryRunner) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield queryRunner.query(`
       DROP TABLE order_products;
       DROP TABLE orders;
       DROP TABLE products;
@@ -60,5 +72,7 @@ export class InitialMigration implements MigrationInterface {
       DROP TABLE credentials;
       DROP TABLE categories;
     `);
-  }
+        });
+    }
 }
+exports.InitialMigration1736259650996 = InitialMigration1736259650996;
