7137ca1cc1ccdf081d53feff3d4c3c6f733fd33e
lbguarino <lbelen2014@gmail.com>
2025-01-08 13:36:09 +0100

    still dev

--- front/src/components/OrderForm/index.tsx
+++ front/src/components/OrderForm/index.tsx
@@ -105,6 +105,7 @@ export default function OrderForm() {
 
     try {
       const token = await getToken();
+     console.log(token);
       const response = await axios.post(
         `${process.env.NEXT_PUBLIC_API_URL}/orders`,
         {
