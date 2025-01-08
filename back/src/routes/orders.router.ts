e1d67213dcdd353de66846d2925eb5ade87c4338
lbguarino <lbelen2014@gmail.com>
2025-01-08 11:13:32 +0100

    final deployment commit

--- back/src/routes/orders.router.ts
+++ back/src/routes/orders.router.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import validateOrderCreate from "../middlewares/orderCreate.middleware";
 import { createOrder } from "../controllers/order.controller";
-import checkLogin from "../middlewares/checkLogin.middleware";
 import { checkJwt } from "../middlewares/auth0.middleware";
 
 const ordersRouter = Router();
