d347d2a8a7194bace5d152c342ef1162c19262de
lbguarino <lbelen2014@gmail.com>
2025-01-08 16:03:48 +0100

    token test

--- front/src/components/OrderForm/index.tsx
+++ front/src/components/OrderForm/index.tsx
@@ -105,7 +105,7 @@ export default function OrderForm() {
 
     try {
       const token = await getToken();
-      console.log(token);
+      console.log("token", token);
       const response = await axios.post(
         `${process.env.NEXT_PUBLIC_API_URL}/orders`,
         {
